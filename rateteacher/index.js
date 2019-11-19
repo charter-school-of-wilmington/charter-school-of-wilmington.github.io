7eeea91738164836b3f7d9a9beb57522c31ec50c
charter-school-of-wilmington <johnnnybart@gmail.com>
2019-11-19 08:10:30 -0500

    moving file structure

--- rateteacher/index.js
+++ rateteacher/index.js
@@ -9,14 +9,30 @@ db.enablePersistence().catch(function(err) {
   }
 });
 
-var teachers = [];
-teachersDB = db.collection("teachers")
-teachersDB.get().then(function(querySnapshot) {
-  querySnapshot.forEach(function(doc) {
-    teachers.push(doc.data());
+$(document).ready(function(){
+  var teachers = [];
+  var exampleteacher = $(".teacher");
+  teachersDB = db.collection("teachers")
+  teachersDB.get().then(function(querySnapshot) {
+    querySnapshot.forEach(function(doc) {
+      teachers.push(doc.data());
+    });
+    
+    for(var i=0;i<teachers.length;i++){
+      var tdata = teachers[i];
+      var tobj = exampleteacher.clone();
+      var telemarr = tobj.get(0).children;
+      
+      telemarr[0].innerHTML = tdata.Name;
+      telemarr[0].href = tdata.Name;
+      //telems[1]. = teacher.Rating;
+      telemarr[2].innerHTML = "Email: " + tdata.Email + "<br>Occupation: " + tdata.Occupation + "<br>Phone: " + tdata.Phone;
+      tobj.css("display", "block");
+      tobj.appendTo(document.body);
+    }
+    
   });
 });
 
-for(var teacher in teachers){
-  
-}
+
+
